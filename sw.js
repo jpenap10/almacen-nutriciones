797ecac04daeb72a66d0dfce2bcbfd45efb2e396
jpenap10 <ah8047667@gmail.com>
2025-09-21 00:28:46 -0500

    Arreglo service worker y manifest para app

--- sw.js
+++ sw.js
@@ -1,14 +1,22 @@
-const CACHE_NAME = 'almacen-v1';
-const urlsToCache = ['/', '/index.html', '/style.css', '/script.js', '/icon-192.png', '/icon-512.png'];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
